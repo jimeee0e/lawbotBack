23c41cc7f129c8cfc9b1f6c4fb3406881e715e74
woon <woon.woon.jo@gmail.com>
2022-08-11 06:05:24 +0900

    ggggggggnnnn

--- src/main/java/bit/project/lawbot/config/WebMvcConfig.java
+++ src/main/java/bit/project/lawbot/config/WebMvcConfig.java
@@ -12,6 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://3.37.220.80:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
